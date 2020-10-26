b0e87916843b46b9fa8801d02e4767a8f115db2d
ArthurPYW <73484631+ArthurPYW@users.noreply.github.com>
2020-10-26 23:40:15 +0800

    20201026_newComre
    摸索Python If, Func(def) 簡易用法
    目標: 猜拳遊戲

---
+++ 001.py
@@ -0,0 +1,47 @@
+# Write your code here :-)
+
+# #=註解
+
+#var
+a = "Hell World"
+Game_001_Start_str = "This is 10/28 HomeWork"
+"""
+weahter = "sunny"
+if weahter == rainy :
+    print("yoyo")
+    else:
+        print("non yoyo")
+
+"""
+
+"""
+#Sample If and else with input
+score = int(input("score:"))
+
+if score >= 60:
+    print("成績及格!")
+else:
+    print("不及格，被當了!")
+"""
+
+GameStart = str(input("Game Start (Y/N):"))
+
+#def Gmae_001 (GameStart) :
+while GameStart != "Y" or "y":
+    if GameStart == "Y":
+    print ("Game Start already")
+elif GameStart == "y" :
+    print("Game Start already_02")
+elif GameStart == "N":
+    print("Allright, game over")
+elif GameStart == "n" :
+    print("All right, game over_02")
+else:
+    #break
+    print("Not answer")
+
+
+#def Game_001 ():
+#if
+#Game_001
+#print(Game_001)
