456609ded8a35fc151fb2dfd6ad60e4611c071cf
PYW <73484631+ArthurPYW@users.noreply.github.com>
2020-11-02 20:33:03 +0800

    導入猜拳的1.四戰三勝 2.電腦一勝與兩勝後 勝率提升基礎功能
    待優化
    1.機率計算array
    2.文字提示
    3.減少重複詢問(開頭設定局數)

--- 001.py
+++ 001.py
@@ -1,25 +1,90 @@
 # Write your code here :-)
-#20201027#
+#20201102#
+#加設 四戰三勝
+#當玩家一勝後, 電腦贏的機率變成50%;  玩家兩勝後, 電腦贏的機率變70%
 # #=註解
 #var
-a = "Hell World"
-Game_001_Start_str = "This is 10/28 HomeWork"
-"""
-weahter = "sunny"
-if weahter == rainy :
-    print("yoyo")
-    else:
-        print("non yoyo")
-"""
+Game_001_Start_str = "This is 10/30 HomeWork"
 
-"""
-#Sample If and else with input
-score = int(input("score:"))
-if score >= 60:
-    print("成績及格!")
-else:
-    print("不及格，被當了!")
-"""
+#可優化, 雛形
+import random
+import numpy as np
+WinnerWording = "You  win  "
+LoseWording = "You Lost?"
+noWnoL = "Drew?"
+PlayerWinnerIndex = 0
+AnsString = "SRPRPS"
+DraftArrayW50 = [0,0,-1,-2]
+DraftArraW70 = [0,0,0,0,0,0,0,-1,-2,-2]
+
+Adv_001 = str(input("Time to guess(Y/N):"))
+while Adv_001.upper() != "END" and PlayerWinnerIndex <= 3:
+    if Adv_001.upper() == "Y" :
+        if PlayerWinnerIndex == 3:
+            break
+        InputAns_001 = str(input("You chose (S/R/P)?: "))
+        #print(AnsString.find(InputAns_001.upper())) #測試String/Array 找目標
+        #print(AnsString[5])#測試STR位子
+
+        if PlayerWinnerIndex == 0 :
+            CompeAns_001 = random.choice("SRP")
+            print("W0 random test")
+        elif PlayerWinnerIndex == 1 :
+            test00 = random.choice(DraftArrayW50)
+            print("W1")
+            print("test00:",test00)
+            CompeAns_001 = AnsString[test00 + AnsString.find(InputAns_001.upper()) +3]
+            print("test00:",test00,"ComAns",CompeAns_001)
+
+        elif PlayerWinnerIndex == 2 :
+            print("W2")
+            test00 = random.choice(DraftArraW70)
+            print("test00:",test00)
+            CompeAns_001 = AnsString[test00 + AnsString.find(InputAns_001.upper()) +3]
+            print("test00:",test00,"ComAns",CompeAns_001)
+        else:
+            break
+        if InputAns_001.upper() == "S" and CompeAns_001 == "P":
+            PlayerWinnerIndex += 1
+            print("Win, it just have",CompeAns_001,"You show",InputAns_001.upper(),WinnerWording,"你勝:",PlayerWinnerIndex,'\n')
+            InpuAns_try = str(input("Wanna Try?(Y/N):"))
+            if InpuAns_try.upper() == "N":
+               break
+        elif InputAns_001.upper() == "R" and CompeAns_001 == "S":
+            PlayerWinnerIndex += 1
+            print("Win, it just have",CompeAns_001,"You show",InputAns_001.upper(),WinnerWording,"你勝:",PlayerWinnerIndex,'\n')
+            InpuAns_try = str(input("Wanna Try?(Y/N):"))
+            if InpuAns_try.upper() == "N":
+               break
+        elif InputAns_001.upper() == "P" and CompeAns_001 =="R":
+            PlayerWinnerIndex += 1
+            print("Win, it just have",CompeAns_001,"You show",InputAns_001.upper(),WinnerWording,"你勝:",PlayerWinnerIndex,'\n')
+            InpuAns_try = str(input("Wanna Try?(Y/N):"))
+            if InpuAns_try.upper() == "N":
+               break
+        elif InputAns_001.upper() == CompeAns_001.upper():
+            print("平手","its answer:",CompeAns_001," and your is :",InputAns_001.upper(),'\n')
+            InpuAns_try = str(input("Wanna Try?(Y/N):"))
+            if InpuAns_try.upper() == "N":
+               break
+        else :
+            print("LostGame","its answer:",CompeAns_001," and your is :",InputAns_001.upper(),'\n')
+            InpuAns_try = str(input("Wanna Try?(Y/N):"))
+            if InpuAns_try.upper() == "N":
+               break
+            #break
+    elif Adv_001.upper() == "N":#對方不開始遊戲時候
+        print("what a pity, you don't start yet, time to END")
+        break
+    #elif Adv_001.upper() == "END":
+        #print("you get it, see u")
+        #break
+    else :
+        print("修理ing")
+        break
+
+print('\n',"EndTest")
+
 
 """
 #雛形
@@ -37,56 +102,6 @@ while GameStart != "test":
             print("Not answer")
             break
 """
-
-#導入002
-import random
-WinnerWording = "You just win the game"
-LoseWording = "You almost win, trt again?"
-noWnoL = "Well, it has same answer with you, try again?"
-
-Adv_001 = str(input("Time to guess(Y/N):"))
-while Adv_001.upper() != "END":
-    if Adv_001.upper() == "Y":
-        InputAns_001 = str(input("You chose (S/R/P)?: "))
-        CompeAns_001 = random.choice("SRP")
-        if InputAns_001.upper() == "S" and CompeAns_001 == "P":
-            print("Win, it just have",CompeAns_001,"You show",InputAns_001.upper(),WinnerWording,'\n')
-            InpuAns_try = str(input("Wanna Try?(Y/N):"))
-            if InpuAns_try.upper() == "N":
-               break
-        elif InputAns_001.upper() == "R" and CompeAns_001 == "S":
-            print("Win, it just have",CompeAns_001,"You show",InputAns_001.upper(),WinnerWording,'\n')
-            InpuAns_try = str(input("Wanna Try?(Y/N):"))
-            if InpuAns_try.upper() == "N":
-               break
-        elif InputAns_001.upper() == "P" and CompeAns_001 =="R":
-            print("Win, it just have",CompeAns_001,"You show",InputAns_001.upper(),WinnerWording,'\n')
-            InpuAns_try = str(input("Wanna Try?(Y/N):"))
-            if InpuAns_try.upper() == "N":
-               break
-        elif InputAns_001.upper() == CompeAns_001.upper():
-            print("平手","its answer:",CompeAns_001," and your is :",InputAns_001.upper(),'\n')
-            InpuAns_try = str(input("Wanna Try?(Y/N):"))
-            if InpuAns_try.upper() == "N":
-               break
-        else :
-            print("LostGame","its answer:",CompeAns_001," and your is :",InputAns_001.upper(),'\n')
-            InpuAns_try = str(input("Wanna Try?(Y/N):"))
-            if InpuAns_try.upper() == "N":
-               break
-            break
-    elif Adv_001.upper() == "N":#對方不開始遊戲時候
-        print("what a pity, you don't start yet, time to END")
-        break
-    #elif Adv_001.upper() == "END":
-        #print("you get it, see u")
-        #break
-    else :
-        print("修理ing")
-        break
-
-print('\n',"EndTest")
-
 
 
 """
