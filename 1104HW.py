0e19b9914172a202ac22b80fefe5db5211e12e97
PYW <73484631+ArthurPYW@users.noreply.github.com>
2020-11-08 23:21:40 +0800

    1105HW_已可以輸出字串by print
    待優化, 需新增STR + append as newSTR and Out as file

---
+++ 1104HW.py
@@ -0,0 +1,53 @@
+"""
+@HW_Due20201104~1108
+1.print輸出以下規律文字(鋸齒圖)內容
+2.輸出成txt/csv/xlcs 等外部文件
+3.prinit出外部文件位址
+
+(請把底線改成空白, 底線只是方便辨識)
+_____*****
+____*****
+___*****
+__*****
+_*****
+*****
+_*****
+__*****
+___*****
+____*****
+_____*****
+"""
+#待優化, 已可以輸出字串by print; 需新增STR + append as newSTR and Out as file
+import numpy as np #適用array
+
+algoIndex = 0
+algoStr = ""
+algoStrLoop = ""
+algoStrIndex = int(input("Input your star count(1~10):"))
+#algoEmpIndex = int(input("Input your Empty count(1~10):"))
+algoIndex = int(input("Input your lucky number(1~10):")) #空白數量
+
+for y in range(algoStrIndex):
+    algoStrLoop = algoStrLoop + "*"
+    #決定你的星星數量, 並固定他as STR
+#print(algoStrLoop)#test
+#print(algoStrIndex)#test
+i = algoIndex #用作下半部測試
+z = algoIndex #用作上半度測試
+b = algoIndex #備用測試
+
+for c in range (b) : #c從0,1,2~b
+    #print('testloop,c:',c,' z:',z)#測試從0開始但希望倒裝
+    z = b - c #倒裝
+    for a in range(z) :
+        print('_',end = '')
+        z -= 1
+    print(algoStrLoop) #空白+星星
+    c -= 1
+
+for i in range(algoIndex + 1) : #input數字會影響這個迴圈走幾次
+    #print(np.repeat('',algoIndex)) #適用array
+    #algoStr = ""
+    for x in range(i) :
+        print('_',end ='')
+    print(algoStrLoop)
