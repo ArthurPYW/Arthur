ea6fdd4826e7f2afc3640fda674f13a943672a1e
PYW <73484631+ArthurPYW@users.noreply.github.com>
2020-11-09 22:50:52 +0800

    1104HW_可以output到特定文件,可以給出位址
    可在優化成-->主動output檔案(不用create檔案) 並同時print位址

--- 1104HW.py
+++ 1104HW.py
@@ -19,6 +19,8 @@ _____*****
 """
 #待優化, 已可以輸出字串by print; 需新增STR + append as newSTR and Out as file
 import numpy as np #適用array
+import os #讀取檔案等套件
+f = open("test.txt","w") #w為覆寫, 打開特定文件覆寫下面內容
 
 algoIndex = 0
 algoStr = ""
@@ -40,14 +42,24 @@ for c in range (b) : #c從0,1,2~b
     #print('testloop,c:',c,' z:',z)#測試從0開始但希望倒裝
     z = b - c #倒裝
     for a in range(z) :
-        print('_',end = '')
+        print('_',end = '', file = f)
         z -= 1
-    print(algoStrLoop) #空白+星星
+    print(algoStrLoop, file = f) #空白+星星
     c -= 1
 
 for i in range(algoIndex + 1) : #input數字會影響這個迴圈走幾次
     #print(np.repeat('',algoIndex)) #適用array
     #algoStr = ""
     for x in range(i) :
-        print('_',end ='')
-    print(algoStrLoop)
+        print('_',end ='', file = f)
+    print(algoStrLoop, file = f)
+
+#print("這是要寫入的資料\n", file = f)
+#print("這是第二行資料\n", file = f)
+f.close()#寫入資料結束
+
+with open("test.txt", "r") as g :#打開特定資料名稱
+    data = g.read()#讀取此檔案
+    print(data)#print檔案
+print(os.getcwd())#獲取當前路徑
+#參考檔案位址網站https://www.itread01.com/content/1542890347.html
